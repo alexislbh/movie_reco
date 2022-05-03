0b5e419a2ffd1dc0f1de29ed2f58da61957732cb
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-03 13:05:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import streamlit as st
-import cv2
+from PIL import Image 
 
+img = Image.open("streamlit.png") 
+  
+st.image(img, width=200) 
 
 df = pd.read_csv("./imdb_movie.csv")  # read a CSV file inside the 'data" folder next to 'app.py'
 # df = pd.read_excel(...)  # will work for Excel files
