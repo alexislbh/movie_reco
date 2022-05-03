3e8f38433f0dd2c5e6bc511a8656f6c2cbd95213
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-03 16:36:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -38,10 +38,12 @@ def knn_reco(ans):
 
   newFilm = pd.DataFrame(columns = imdb.columns) 
 
-  for i in range(6):
-    newFilm.append(imdb.iloc[predict[1][0][i],:])
+  #for i in range(6):
+  #  newFilm = newFilm.append(imdb.iloc[predict[1][0][i],:])
+  newFilm = [imdb.iloc[predict[1][0][i],:] for i in range(6)]
   return newFilm
 
+  
 
 newFilm = knn_reco(ans)
 st.write(newFilm)
