0f1b01e6d308edef3588290973f6ed070d0a942f
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-03 10:44:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 
 st.title('Movie reco')
-  
-DATA_URL = ('https://github.com/Pilouliz/movie_reco/blob/main/imdb_movie.zip')
 
-st.dataframe(DATA_URL)
+imdb = pd.read_csv('https://github.com/Pilouliz/movie_reco/blob/main/imdb_movie.zip')
+
+st.dataframe(imdb)
