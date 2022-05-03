4825c08c58bc54b587a707417ee6d5fd8a1616b6
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-03 14:00:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ df = pd.read_csv("./imdb_movie.csv")  # read a CSV file inside the 'data" folder
 st.title("Movie Reco")  # add a title
 #st.write(df)  # visualize my dataframe in the Streamlit app
 
-
-ans = st.selectbox ('Votre film préféré', df.title, index=6040)
 img_ans = df.poster_url[df.title==ans]
 st.image(img_ans.values[0], width = 200)
+ans = st.selectbox ('Votre film préféré', df.title, index=6040)
+
 
 
