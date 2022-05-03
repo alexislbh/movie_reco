d3c3b6ca9009c9ec021face9722fdded18c230bd
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-03 10:41:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,4 @@ st.title('Movie reco')
   
 DATA_URL = ('https://github.com/Pilouliz/movie_reco/blob/main/imdb_movie.zip')
 
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    return data
-  
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text('Loading data...done!')
+st.dataframe(DATA_URL)
