0d944d9df8bafe93396ec1f32743310416c8621b
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-03 11:46:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,9 @@ import numpy as np
 import streamlit as st
 
 
-st.title('Movie reco')
+df = pd.read_csv("./data/titanic.csv")  # read a CSV file inside the 'data" folder next to 'app.py'
+# df = pd.read_excel(...)  # will work for Excel files
 
-@st.cache
-def load_data():
-   imdb = pd.read_csv('Pilouliz-data/imdb_movie.csv')
-   return imdb
-
-df = load_data()
-st.write(df)
+st.title("Hello world!")  # add a title
+st.write(df)  # visualize my dataframe in the Streamlit app
 
