0d9691d5fa7ee1e86ec579699e88911582b4480f
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-03 16:11:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def knn_reco(ans):
 st.write(knn_reco(ans))
 
 for i in range(1,6):
-    if pd.isna(newFilm.poster_url[:6].values[i]) == False:
-        st.image(newFilm.poster_url[:6].values[i], width = 200)
+    if pd.isna(newFilm.poster_url.values[i]) == False:
+        st.image(newFilm.poster_url.values[i], width = 200)
     else:
         st.image('https://upload.wikimedia.org/wikipedia/commons/e/e6/Pas_d%27image_disponible.svg', width = 200)
