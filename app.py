93a3f9403142d967beee2bab819a6606ae4bedb4
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-03 10:53:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ st.title('Movie reco')
 
 #st.dataframe(imdb)
 
-df = pd.DataFrame(
-    np.random.randn(50, 20),
-    columns=('col %d' % i for i in range(20)))
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
 
-st.dataframe(df)
+df
