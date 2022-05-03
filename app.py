877e7c61733ee61cfa5e5afb6574b280e18c8f91
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-03 10:36:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 import pandas as pd
 import streamlit as st
 
-st.title('Uber pickups in NYC')
+st.title('Movie reco')
   
-imdb = pd.read_csv('imdb_movie.zip')
-
 DATA_URL = ('https://github.com/Pilouliz/movie_reco/blob/main/imdb_movie.zip')
 
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
     return data
