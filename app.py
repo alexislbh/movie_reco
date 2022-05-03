e151bad47e82f0229276c45dfc796023026f281c
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-03 14:01:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,9 @@ import numpy as np
 import streamlit as st
 from PIL import Image 
 
-df = pd.read_csv("./imdb_movie.csv")  # read a CSV file inside the 'data" folder next to 'app.py'
-# df = pd.read_excel(...)  # will work for Excel files
+df = pd.read_csv("./imdb_movie.csv") 
 
-st.title("Movie Reco")  # add a title
-#st.write(df)  # visualize my dataframe in the Streamlit app
+st.title("Movie Reco") 
 
 ans = st.selectbox ('Votre film préféré', df.title, index=6040)
 img_ans = df.poster_url[df.title==ans]
