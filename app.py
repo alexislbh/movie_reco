ebead9c5b7c81153abcd90280c4267d6f6acdd79
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-03 10:50:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ import streamlit as st
 
 st.title('Movie reco')
 
-imdb = pd.read_csv('https://github.com/Pilouliz/movie_reco/blob/main/imdb_movie.csv')
+#imdb = pd.read_csv('https://github.com/Pilouliz/movie_reco/blob/main/imdb_movie.csv')
 
-st.dataframe(imdb)
+#st.dataframe(imdb)
+
+df = pd.DataFrame(
+    np.random.randn(50, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(df)
