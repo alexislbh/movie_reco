eea19570baeab0b658ea7502bb7bea552e3031c8
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-03 14:52:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ st.title("Movie Reco")
 
 ans = st.selectbox ('Votre film préféré', df.title, index=6040)
 img_ans = df.poster_url[df.title==ans]
-st.image(img_ans.values[0], width = 200)
+img = img_ans.apply(lambda X: 'https://upload.wikimedia.org/wikipedia/commons/e/e6/Pas_d%27image_disponible.svg' if pd.isna(X) == True else img_ans.values[0])
+st.image(img, width = 200)
+  
 
 
