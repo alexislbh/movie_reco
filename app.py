db103dc37e3fb5b39c5621878439e71f235a61a1
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-03 11:51:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 import streamlit as st
 
 
-df = pd.read_csv("./data/titanic.csv")  # read a CSV file inside the 'data" folder next to 'app.py'
+df = pd.read_csv("./imdb_movie.csv")  # read a CSV file inside the 'data" folder next to 'app.py'
 # df = pd.read_excel(...)  # will work for Excel files
 
 st.title("Hello world!")  # add a title
