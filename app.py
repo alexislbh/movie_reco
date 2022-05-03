8027e18d83bcf831fe649312b4743189e46c1f52
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-03 12:04:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import streamlit as st
 df = pd.read_csv("./imdb_movie.csv")  # read a CSV file inside the 'data" folder next to 'app.py'
 # df = pd.read_excel(...)  # will work for Excel files
 
-st.title("Hello world!")  # add a title
+st.title("Movie Reco")  # add a title
 #st.write(df)  # visualize my dataframe in the Streamlit app
 
 st.multiselect('Votre film préféré', df.title)
