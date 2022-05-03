4a681bab0709b7c55b07d1faed7cbe6dd2e10429
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-03 15:12:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,4 +14,31 @@ if pd.isna(img_ans.values[0]) == False:
 else:
   st.image('https://upload.wikimedia.org/wikipedia/commons/e/e6/Pas_d%27image_disponible.svg', width = 200)
 
+#KNN
+def knn_reco(ans)
+  X = imdb[['isAdult', 'startYear', 'runtimeMinutes', 'averageRating', 'numVotes', 'Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'History', 'Horror', 'Music', 'Musical', 'Mystery', 'News', 'Romance', 'Sci-Fi', 'Sport', 'Thriller', 'War', 'Western','original_language']]
 
+  scale = StandardScaler().fit(X) 
+  X_scaled = scale.transform(X)
+  X_scaled = scale.transform(X)
+
+  x_scaled = pd.DataFrame(X_scaled, columns=X.columns)
+
+  x_scaled['numVotes'] = x_scaled.numVotes * 1.5
+  x_scaled['startYear'] = x_scaled.startYear * 1.2
+  x_scaled.iloc[:,5:] = x_scaled.iloc[:,5:] * 1.2
+  x_scaled['averageRating'] = x_scaled.averageRating * 0.8
+  x_scaled['original_language'] = x_scaled.original_language * 2
+
+  distanceKNN = NearestNeighbors(n_neighbors=6).fit(X_scaled)
+
+  predict = distanceKNN.kneighbors(X_scaled[imdb.title == ans)
+
+  newFilm = pd.DataFrame(columns = imdb.columns) 
+
+  for i in range(6):
+    newFilm = newFilm.append(imdb.iloc[predict[1][0][i],:])
+    #if i !=0 : print(np.array(newFilm.title)[i]) # Affiche que le nom des films
+  return newFilm #Debug
+                                            
+st.write(knn_reco(ans))
