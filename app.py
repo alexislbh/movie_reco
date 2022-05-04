a3f1a21e43da7c13010e4b3460caf03da4b8a33d
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-04 16:43:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ reco_val = slider_val + 1
 ans = st.selectbox ('Votre film préféré', imdb.title, index=6040)
 
 with st.sidebar:
-  #st.markdown("<h1 style='text-align: center; color: grey;'>st.subheader(imdb.title[imdb.title==ans].values[0])</h1>", unsafe_allow_html=True)
-  st.markdown("<div style='text-align: center'>st.subheader(imdb.title[imdb.title==ans].values[0])</div>")
+  [random value]: st.subheader(imdb.title[imdb.title==ans].values[0])
+  st.markdown("<h1 style='text-align: center; color: grey;'>[random value]</h1>", unsafe_allow_html=True)
   img_ans = imdb.poster_url[imdb.title==ans]
   if pd.isna(img_ans.values[0]) == False:
     st.image(img_ans.values[0], use_column_width=True)
