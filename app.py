b941f7414e4cbe05be4e9ed5c3cabca8bd482dde
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-04 10:33:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -55,9 +55,9 @@ st.write(newFilm)
 step_range = sum([slider_val//5 if slider_val%5==0 else slider_val//5 +1])
 
 for steps in range(step_range):
-  st.write(steps)
+  next_line = steps * 5
   cols = st.columns(slider_val)
-  for num in range(1,reco_val):
+  for num in range(1 + next_line,reco_val + next_line):
     if pd.isna(newFilm.poster_url.values[num]) == False:
       cols[num-1].image(newFilm.poster_url.values[num], width = 100)
     else:
