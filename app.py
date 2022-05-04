4d25322fe9b27e671ecd238f1e8379789523211b
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-04 09:33:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ imdb = pd.read_csv("./imdb_movie.csv")
 imdb['original_language'] = pd.factorize(imdb.original_language)[0]
 
 st.title("Movie Reco") 
-slider_val = st.slider('Choose your number of recomendation', 1, 15, value=5)
+slider_val = st.slider('Choose your number of recomendation', 1, 15, value=5) + 1
+st.write(slider_val)
 ans = st.selectbox ('Votre film préféré', imdb.title, index=6040)
 
 with st.sidebar:
