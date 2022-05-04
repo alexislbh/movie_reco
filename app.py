82004f12708a3ec2f3b50a54f17f5b8c5ec03706
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-04 11:58:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ with st.sidebar:
     st.image(img_ans.values[0], width = 200)
   else:
     st.image('https://upload.wikimedia.org/wikipedia/commons/e/e6/Pas_d%27image_disponible.svg', width = 200)
-  cols1, clos2 = st.columns(2)
+  cols1, cols2 = st.columns(2)
   cols1.write(imdb.averageRating[imdb.title==ans].values[0])
   cols2.write(imdb.startYear[imdb.title==ans].values[0])
 
