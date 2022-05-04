1cd4a670cc27d8ff033349aa9daed5e206037271
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-04 16:38:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ ans = st.selectbox ('Votre film préféré', imdb.title, index=6040)
 
 with st.sidebar:
   #st.markdown("<h1 style='text-align: center; color: grey;'>st.subheader(imdb.title[imdb.title==ans].values[0])</h1>", unsafe_allow_html=True)
-  st.markdown(<div style='text-align: center'>st.subheader(imdb.title[imdb.title==ans].values[0])</div>)
+  st.markdown("<div style='text-align: center'>st.subheader(imdb.title[imdb.title==ans].values[0])</div>")
   img_ans = imdb.poster_url[imdb.title==ans]
   if pd.isna(img_ans.values[0]) == False:
     st.image(img_ans.values[0], use_column_width=True)
