bc6676daf89bc7b08ca9f811ff783133342bb304
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-04 22:15:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -82,4 +82,5 @@ for steps in range(step_range):
     cols[(num-1) - next_line].markdown("<p style='text-align: center'>{}</p>".format(newFilm.title.values[num]), unsafe_allow_html=True)
     cols[(num-1) - next_line].markdown("<p style='text-align: center'><b>{}</b><font color='black'> - - - - </font><i>{}</i></p>".format(newFilm.averageRating.values[num], newFilm.startYear.values[num]), unsafe_allow_html=True)
 key = st.secrets["key"]
+st.write(key)
 st.image('http://www.omdbapi.com/?apikey={}&i=tt1285016'.format(st.secrets["key"]), width = 200)
