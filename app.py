62318ce670b4a27ff416303d6bcb270908248020
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-05 18:20:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from PIL import Image
 
 st.set_page_config(layout="wide", initial_sidebar_state="expanded")
 
-imdb = pd.read_pickle('./imdb_movie').pickle.dump(protocol=4)
+imdb = pickle.dump(pd.read_pickle('./imdb_movie'), protocol=4)
 
 st.title("Movie Reco") 
 
