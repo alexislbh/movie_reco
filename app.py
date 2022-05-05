f5e2de5a09a3cb10e7c7c769f6aa91bab0982098
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-05 17:20:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import pickle
 import requests
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import NearestNeighbors
