ee952a87a0f021afaff69be47430d753e85762db
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-09 14:28:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -79,3 +79,4 @@ for steps in range(step_range):
     cols[(num-1) - next_line].markdown("<p style='text-align: center'><b>{}</b><font color='black'> - - - - </font><i>{}</i></p>".format(round(newFilm.averageRating.values[num],1), newFilm.startYear.values[num]), unsafe_allow_html=True)
     cols[(num-1) - next_line].image(get_OMDB(newFilm.tconst.values[num])['Poster'], use_column_width = 'auto')
     cols[(num-1) - next_line].markdown("<p style='text-align: center'>{}</p>".format(newFilm.title.values[num]), unsafe_allow_html=True)
+    cols[(num-1) - next_line].button('Select')
