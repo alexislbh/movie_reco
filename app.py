9598802da44d6a643ff45da3b7779c71dedcf832
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-10 00:21:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import requests
+import webbrowser
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import NearestNeighbors
 from PIL import Image
