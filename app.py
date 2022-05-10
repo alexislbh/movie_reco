85d348600bf96c68a5b3847a64f7ab7fd991d7a6
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-10 14:26:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -31,8 +31,11 @@ imdb_movie = pd.read_pickle('./imdb_movie.pkl')
 imdb_original_language = pd.read_pickle('./imdb_original_language.pkl')
 imdb = pd.merge(imdb_movie, imdb_original_language, how="left", on=["tconst"])
 
-setting_name = ['Num Vote','Year','Genres','Rating','Region','Directors','Keyword']
+setting_name = ['Num Vote','Year','Genres','Rating','Region','Directors','Keyword','Actors']
 settings =[1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0]
+#settings = {'Num Vote':1.0
+#            
+#           }
 
 with st.sidebar:
 #if False:
