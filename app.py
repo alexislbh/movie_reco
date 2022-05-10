ca18883a79448b43763379391bfca78fcfb9fe6f
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-10 10:03:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,17 @@ from PIL import Image
 #st.button('Select',on_click=('https://share.streamlit.io/oscararnoux8/projet2_wcs/main/project2_viz.py'))
 #st.markdown('Essayez notre [algorithme de recommandation](https://share.streamlit.io/oscararnoux8/projet2_wcs/main/project2_viz.py)')
 
-st.set_page_config(layout="wide", initial_sidebar_state="expanded")
+st.set_page_config(
+     page_title="Algo recomandation",
+     page_icon="🦾",
+     layout="wide",
+     initial_sidebar_state="expanded",
+     menu_items={
+         'Data Viz': 'https://share.streamlit.io/oscararnoux8/projet2_wcs/main/project2_viz.py',
+         'Report a bug': "https://www.extremelycoolapp.com/bug",
+         'About': "# This is a header. This is an *extremely* cool app!"
+     }
+ )
 
 st.write(f'''
     <a target="_self" href="https://share.streamlit.io/oscararnoux8/projet2_wcs/main/project2_viz.py" style="text-decoration: none">
