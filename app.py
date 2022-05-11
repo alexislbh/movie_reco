a552ec13dd7d787a681776016e5853cdcaaac3d0
Pilouliz <100358051+Pilouliz@users.noreply.github.com>
2022-05-11 11:08:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def get_OMDB(movieID):
 
 cols = st.columns(len(setting_name))
 #for i in range(len(setting_name)):
-for keys in avis_w.keys()
+for keys in avis_w.keys():
   key = cols[i].selectbox(setting_name[i],keys)
   settings[i] = avis_w[key] 
   #settings[i] = cols[i].number_input(setting_name[i],value=settings[i],step=0.1)
